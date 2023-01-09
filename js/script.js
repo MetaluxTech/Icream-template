ae7272f8e5c1f501849cd27812ab268b341d1f67
Noor <nonona319@gmail.com>
2023-01-08 18:30:58 -0900

    noor

---
+++ js/script.js
@@ -0,0 +1,58 @@
+//slider image
+let navbar = document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu-btn');
+
+menuBtn.onclick = () =>{
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menuBtn.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+
+var swiper = new Swiper(".home-slider", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true,
+    },
+    grabCursor:true,
+    loop:true,
+    spaceBetween:20,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+
+//button
+document.querySelectorAll('.button').forEach(button => button.addEventListener('click', e => {
+                        if(!button.classList.contains('loading')) {
+                    
+                            button.classList.add('loading');
+                    
+                            setTimeout(() => button.classList.remove('loading'), 3700);
+                    
+                        }
+                        e.preventDefault();
+                    }));
